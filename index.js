40c89738a7c13edeb43180f91a7c174a3fc17255
GenisFM <168083280+GenisFM@users.noreply.github.com>
2024-05-17 11:54:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function canvia_seccio(num_boto) {
 }
 let validat = false;    // variable que permet saber si hi ha algun usuari validat
 let nom, contrasenya;
-let scriptURL = "https://script.google.com/.../exec"    // s'ha de substituir la cadena de text per la URL del script
+let scriptURL = "https://script.google.com/macros/s/AKfycbzKQuqdqJXG2VsWy_ah1ZXWi-f33VNPuS42Ml35JRgzZqlOHmswhBHd9-Lri4OXy8gN/exec"    // s'ha de substituir la cadena de text per la URL del script
 
 function inici_sessio() {
     nom = document.getElementById("nom_usuari").value;    // la propietat "value" d'un quadre de text correspon al text escrit per l'usuari
